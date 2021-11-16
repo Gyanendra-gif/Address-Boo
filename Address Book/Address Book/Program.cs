09921eee54c87aa1da40767152cb874c7a5de449
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-16 10:47:47 +0530

    [Gyanendra] Add: A Program of Contacts

---
+++ Address Book/Address Book/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Address_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Address Program Press- 1-Contacts, 2-Add Contact, 3-Edit Contact, 4-Delete Contact, 5-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact Information in Format of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        Contact contact = new Contact();
+                        contact.FirstName = Console.ReadLine();
+                        contact.LastName = Console.ReadLine();
+                        contact.Address = Console.ReadLine();
+                        contact.City = Console.ReadLine();
+                        contact.State = Console.ReadLine();
+                        contact.Zip = Console.ReadLine();
+                        contact.PhoneNumber = Console.ReadLine();
+                        contact.Email = Console.ReadLine();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
