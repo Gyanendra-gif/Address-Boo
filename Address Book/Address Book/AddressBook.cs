ce5d6bd1e8be4d4acaf657c05ccc73ab95b0b383
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-08 13:49:51 +0530

    [Gyanendra] Refactor: A Program to Edit a Contact

--- Address Book/Address Book/AddressBook.cs
+++ Address Book/Address Book/AddressBook.cs
@@ -31,7 +31,61 @@ namespace Address_Book
             {
                 if (contact.FirstName == name || contact.LastName == name)
                 {
-                    Console.WriteLine("What is Required to be Edited");
+                    bool flag = true;
+                    while (flag == true)
+                    {
+                        Console.WriteLine("What to be Edited:\n1.firstname\n2.lastname\n3.address\n4.city\n5.state\n6.zip\n7.phone.no\n8.email-id\n9.exit");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                string Firstname = Console.ReadLine();
+                                contact.FirstName = Firstname;
+                                Display();
+                                break;
+                            case 2:
+                                string Lastname = Console.ReadLine();
+                                contact.LastName = Lastname;
+                                Display();
+                                break;
+                            case 3:
+                                string address = Console.ReadLine();
+                                contact.Address = address;
+                                Display();
+                                break;
+                            case 4:
+                                string City = Console.ReadLine();
+                                contact.City = City;
+                                Display();
+                                break;
+                            case 5:
+                                string State = Console.ReadLine();
+                                contact.State = State;
+                                Display();
+                                break;
+                            case 6:
+                                string Zip = Console.ReadLine();
+                                contact.Zip = Zip;
+                                Display();
+                                break;
+                            case 7:
+                                string PhoneNo = Console.ReadLine();
+                                contact.PhoneNumber = PhoneNo;
+                                Display();
+                                break;
+                            case 8:
+                                string emailid = Console.ReadLine();
+                                contact.Email = emailid;
+                                Display();
+                                break;
+                            case 9:
+                                flag = false;
+                                break;
+                            default:
+                                Console.WriteLine("your choice should be between 1 to 9");
+                                break;
+                        }
+                    }
                 }
             }
         }
